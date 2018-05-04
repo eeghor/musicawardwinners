0376765a2af7149a3013e9cddbfbc255410339f8
Igor Korostil <eeghor@gmail.com>
2018-05-04 12:11:26 +1000

    reasonably final

--- maws.py
+++ maws.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 import os
 import time
 from collections import defaultdict
+import json
 
 class AwardWinners:
 
@@ -15,32 +16,33 @@ class AwardWinners:
 	AWARDS = {k: {'url': None, 
 					'artists': set()} 
 					for k in 
-			  			['americana music honors & awards',    # since 2002
-			  			'anugerah',           # since 1997
-			  			'australasian performing right association music award',     # apra
-			  			'australian recording industry association music award',
-			  			'brit awards',
-			  			'echo music prize',
+			  			['americana music honors & awards',
+			  			'anugerah',       
+			  			'apra award',     
+			  			'aria award',
+			  			'brit award',
+			  			'echo',
 			  			'eurovision',
 			  			'golden globe',
 			  			'grammy',
 			  			'independent music awards',
 			  			'kerrang! awards',
 			  			'korean music award',
-			  			'latin grammy award',
+			  			'latin grammy',
 			  			'melon music award',
 			  			'mnet asian music award',
 			  			'nme awards',
 			  			'pacific music award',
 			  			'world music awards']}
 
+	ARTISTS = defaultdict(list)
+
 	def __init__(self):
 		
-		self.ARTISTS = defaultdict(set)
+		self.STORAGE_DIR = 'collected'
 
 	def get(self):
 
-		# ChromeDriver is 2.38 available at https://chromedriver.storage.googleapis.com/2.38/chromedriver_mac64.zip
 		self.DRIVER = webdriver.Chrome('webdriver/chromedriver')
 
 		self.DRIVER.get(AwardWinners.URL)
@@ -48,43 +50,84 @@ class AwardWinners:
 		h = self.DRIVER.find_element_by_id('mw-subcategories')
 
 		for _ in h.find_elements_by_class_name('CategoryTreeItem'):
+
+			awards_ = []
+
 			for a in AwardWinners.AWARDS:
-				if a.lower() in _.text.lower():
+				if a.lower() in _.text.lower().strip():
 					l = _.find_element_by_xpath('a').get_attribute('href')
-					AwardWinners.AWARDS[a]['url'] = l
-					break
+					awards_.append((a, l))
+
+			if awards_:
+
+				a, _ = max(awards_, key=lambda x: len(x[0].split()))
+				AwardWinners.AWARDS[a]['url'] = _
 
 		for k in AwardWinners.AWARDS:
+			if not AwardWinners.AWARDS[k]['url']:
+				print(AwardWinners.AWARDS[k]['url'])
+
+		for k in AwardWinners.AWARDS:
+
+			print(f'award: {k}...')
 
 			try:
 				self.DRIVER.get(AwardWinners.AWARDS[k]['url'])
 			except:
 				continue
 
-			for p in self.DRIVER.find_elements_by_class_name('mw-category-group'):
+			scrape_ = True
 
-				letter = None
+			while scrape_:
 
+				div_ = self.DRIVER.find_element_by_id('mw-pages')
+	
+				for p in div_.find_elements_by_class_name('mw-category-group'):
+	
+					isLetter = False
+	
+					try:
+						isLetter = p.find_element_by_xpath('.//h3').text.strip()
+					except:
+						continue
+	
+					if isLetter and isLetter.replace('-','').isalnum():
+						for i in p.find_elements_by_xpath('.//li'):
+							AwardWinners.AWARDS[k]['artists'].add(i.text.lower().split('(')[0].strip())
+					else:
+						# go to the next block
+						continue
+	
+				# is there next page
 				try:
-					letter = p.find_element_by_xpath('h3').text.strip()
+					next_page_ = div_.find_element_by_partial_link_text('next page')
+					self.DRIVER.get(next_page_.get_attribute('href'))
 				except:
-					continue
-
-				if letter and letter.isalnum():
-					for i in p.find_elements_by_xpath('.//li'):
-						AwardWinners.AWARDS[k]['artists'].add(i.text.lower().strip())
-				else:
-					continue
-				
-		print(AwardWinners.AWARDS)
+					scrape_ = False
 
 
 		self.DRIVER.close()
 
+		# fill in a dictionary with artists as keys and awards as a list of values 
+
+		for award in AwardWinners.AWARDS:
+			for artist in AwardWinners.AWARDS[award]['artists']:
+				AwardWinners.ARTISTS[artist].append(award)
+
 		return self
+
+	def save(self, f):
+
+		if not os.path.exists(self.STORAGE_DIR):
+			os.mkdir(self.STORAGE_DIR)
+
+		json.dump(AwardWinners.ARTISTS, open(f'{self.STORAGE_DIR}/{f}','w'))
+
+		print(f'saved {len(AwardWinners.ARTISTS)} artists to {f}')
 
 if __name__ == '__main__':
 
 	aw = AwardWinners()
 	aw.get()
+	aw.save('award_winners.json')
 
